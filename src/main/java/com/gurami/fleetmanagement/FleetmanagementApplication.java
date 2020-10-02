6eb9515a7884170d1e9109d5b31201775ea56700
Gurami khatiashvili <guramxatiashvili@gmail.com>
2020-10-02 23:00:26 +0200

    change paths to static files for spring boot to serve css js

--- src/main/java/com/gurami/fleetmanagement/FleetmanagementApplication.java
+++ src/main/java/com/gurami/fleetmanagement/FleetmanagementApplication.java
@@ -1,12 +1,17 @@
 package com.gurami.fleetmanagement;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+
+import javax.xml.crypto.dsig.XMLValidateContext;
 
 @SpringBootApplication
-public class FleetmanagementApplication {
 
-	public static void main(String[] args) {
+public class FleetmanagementApplication extends WebMvcAutoConfiguration {
+
+    public static void main(String[] args) {
 		SpringApplication.run(FleetmanagementApplication.class, args);
 	}
 
