299daa6a0ff03250acbe8cfebf376849e7cc2236
Gurami khatiashvili <guramxatiashvili@gmail.com>
2020-10-03 20:53:04 +0200

    background img update and added country controller

--- src/main/java/com/gurami/fleetmanagement/controllers/CountryController.java
+++ src/main/java/com/gurami/fleetmanagement/controllers/CountryController.java
@@ -1,7 +1,13 @@
 package com.gurami.fleetmanagement.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CountryController {
+
+    @GetMapping("/countries")
+    public String getCountries(){
+        return "country";
+    }
 }
