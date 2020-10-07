e486b4b32087440592e4a654f01ae975344961d9
Gurami khatiashvili <guramxatiashvili@gmail.com>
2020-10-07 14:37:32 +0200

    list countries from DB into HTML

--- src/main/java/com/gurami/fleetmanagement/controllers/CountryController.java
+++ src/main/java/com/gurami/fleetmanagement/controllers/CountryController.java
@@ -1,13 +1,25 @@
 package com.gurami.fleetmanagement.controllers;
 
+import com.gurami.fleetmanagement.models.Country;
+import com.gurami.fleetmanagement.services.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CountryController {
 
+    @Autowired
+    private CountryService countryService;
+
     @GetMapping("/countries")
-    public String getCountries(){
+    public String getCountries(Model model){
+        List<Country> countryList = countryService.getCountries();
+        //pass list of countries to html
+        model.addAttribute("countries",countryList);
         return "country";
     }
 }
--- src/main/java/com/gurami/fleetmanagement/services/CountryService.java
+++ src/main/java/com/gurami/fleetmanagement/services/CountryService.java
@@ -1,7 +1,20 @@
 package com.gurami.fleetmanagement.services;
 
+import com.gurami.fleetmanagement.models.Country;
+import com.gurami.fleetmanagement.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    //This will return list of countries
+    public List<Country> getCountries(){
+        return countryRepository.findAll();
+    }
 }
